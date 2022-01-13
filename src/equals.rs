16936c0dc5d1c465877b92425d442e081a883751
ixap2i <ayamashita124@gmail.com>
2022-01-13 13:14:47 +0900

    fix import statement

---
+++ src/equals.rs
@@ -0,0 +1,27 @@
+pub mod equals {
+  pub fn call_nested_equals() {
+		self::nested_equals::option();
+  }
+  // 2^8で構成された配列の中身が同じかを判定する処理
+  pub fn equals_array(a: Vec<u8>, b: Vec<u8>) -> bool {
+		if a.len() != b.len() {
+				return false;
+		}
+		let mut iter_a = a.iter();
+		let mut iter_b = a.iter();
+		while iter_a.len() > 0 {
+			if iter_a.next() != iter_b.next() {
+				return false;
+			}
+		}
+		return true;
+  }
+  pub mod nested_equals {
+		pub fn equals_by_nested_equals() {
+			print!("nothing");
+		}
+		pub(in super) fn option() {
+			print!("only visible from equals");
+		}
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-// use crate::equals::equals_array;
-// use crate::equals::nested_equals::equals_by_nested_equals;
-// use crate::equals::nested_equals::option;
-// use crate::equals::call_nested_equals;
+use hello_cargo::equals::equals::equals_array;
+use hello_cargo::equals::equals::call_nested_equals;
 extern crate hello_cargo;
 fn main() {
-    // let a: [u8; 3] = [1, 2, 3];
-    // let b: [u8; 3] = [1, 2, 3];
-    let a = vec![1, 2, 3];
-    let b = vec![1, 2, 3];
-    // print!!("{}", equals_array(a, b));
-    println!("{}", hello_cargo::equals::equals_array(a, b));
-    hello_cargo::equals::call_nested_equals();
+  let a = vec![1, 2, 3];
+  let b = vec![1, 2, 3];
+  println!("{}", equals_array(a, b));
+  call_nested_equals();
 }
