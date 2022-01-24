bfd6f09678b43ad948ae95d5cf5f94c58a0211d7
ixap2i <ayamashita124@gmail.com>
2022-01-24 14:03:06 +0000

    develop initial commit

---
+++ app/src/main.rs
@@ -0,0 +1,15 @@
+/*--------------------------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *-------------------------------------------------------------------------------------------------------------*/
+
+struct Draft {
+   id: String,
+   title: String,
+   body: String,
+   published: bool
+}
+
+fn main() {
+    println!("{}", "hello_cargo!".to_string())
+}
