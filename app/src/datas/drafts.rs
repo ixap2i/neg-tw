001028542f8a24cb152ac358ecb9c6b5bdec70c6
ixap2i <ayamashita124@gmail.com>
2022-02-03 21:09:08 +0900

    add data dir

---
+++ app/src/datas/drafts.rs
@@ -0,0 +1,8 @@
+# [derive(Clone)]
+pub struct Draft {
+  pub id: i8,
+  pub title: String,
+  pub body: String,
+  pub link: String,
+  pub published: bool
+}
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,18 +1,11 @@
+pub mod datas;
+use datas::drafts::Draft;
 /*--------------------------------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
-
-struct Draft {
-   id: i8,
-   title: String,
-   body: String,
-   link: String,
-   published: bool
-}
-
-fn main() {
-    let mut draft_obj = vec![
+ fn main() {
+    let draft_obj = vec![
         Draft {
             id: 1i8,
             title: "this is title".to_string(),
