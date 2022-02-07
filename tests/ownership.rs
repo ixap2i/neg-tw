cbb401ae9dbb33fd495dff5a96e0cacc8fc2ff50
ixap2i <ayamashita124@gmail.com>
2022-02-07 11:49:02 +0000

    add about ownership test

---
+++ tests/ownership.rs
@@ -0,0 +1,34 @@
+#[test]
+fn borrow_variable() {
+    let mut test = "test";
+
+    // testを共有参照している(参照の代入)
+    let t = &test;
+    // testを可変参照している
+    // let t = &mut test;
+    // 8行目がコメントアウトされている場合、可変オブジェクトとして借用しているため、print内で再度借用することは出来ない
+    print!("{}", test);
+    print!("{}", t);
+}
+
+fn local_scope() {
+    {
+        let out_of_scope;
+        {
+            let inner_scope = 1;
+            // 借用された値は十分に生存できないとエラーが出る
+            // ブロックを出るとinner_scopeはしぬ
+            out_of_scope = &inner_scope;
+            // 2. inner_scopeと同じ生存期間で使用されているのでOK
+            assert_eq!(out_of_scope, &1);
+        }
+        // 1. だめ
+        // assert_eq!(out_of_scope, 1);
+    }
+}
+
+fn life_time() {
+    fn print_v<'a>(target_v: &'a i32) {  print!("{}", target_v); }
+    let vv = 10;
+    print_v(&vv);
+}
