35af595078fbe8c9771fe2d250e73d32751c9192
ixap2i <ayamashita124@gmail.com>
2022-02-27 19:04:20 +0900

    add Dog type for serializable json

--- tests/err_handling.rs
+++ tests/err_handling.rs
@@ -1,25 +1,26 @@
-use std::{collections::HashMap, error::Error};
+use serde::{Serialize, Deserialize, de::Error};
 
-# [derive(Clone, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Dog {
-  pub message: String,
-  pub status: String,
+    pub message: String,
+    pub status: String,
 }
 
 #[tokio::test]
 async fn error_handling() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://dog.ceo/api/breeds/image/random")
+    let resp = reqwest::get("https://dog.ceo/api/breeds/image/rando")
         .await?
-        .json::<HashMap<String, String>>()
+        .json::<Dog>()
         .await?;
+
     // panicked at equal
     // TODO: Error is unreachable pattern
     match &resp {
-        Result => {
-            println!("{:#?}", &resp);
+        result => {
+            println!("dog image url: {:#?}", result.message);
         },
         Error => {
-            println!("{:#?}", "err!");
+            println!("err: {:#?}", &resp);
         }
     }
     Ok(())
