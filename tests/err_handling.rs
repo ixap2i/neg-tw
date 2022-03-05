6cf049c2286f588ed75ae66f7ebfa827c052e76e
ixap2i <ayamashita124@gmail.com>
2022-03-05 17:08:57 +0900

    pending implement error handling

--- tests/err_handling.rs
+++ tests/err_handling.rs
@@ -1,9 +1,39 @@
-use serde::{Serialize, Deserialize, de::Error};
+use serde::{Serialize, Deserialize};
+use std::fmt::{self, Debug};
+use std::error;
+use std::error::Error;
+use std::any::type_name;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Dog {
     pub message: String,
     pub status: String,
+}
+
+pub enum JsonResult {
+    Dog,
+    Error
+}
+
+impl fmt::Display for Dog {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "json response has error")
+    }
+}
+// TODO: かきこむ
+impl error::Error for Dog {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+pub mod my_crate {
+    pub struct Dog;
+}
+
+// TODO: type assertion println!("dog image url: {:#?}", type_of(&resp));
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
 }
 
 #[tokio::test]
@@ -13,15 +43,9 @@ async fn error_handling() -> Result<(), Box<dyn std::error::Error>> {
         .json::<Dog>()
         .await?;
 
-    // panicked at equal
-    // TODO: Error is unreachable pattern
-    match &resp {
-        result => {
-            println!("dog image url: {:#?}", result.message);
-        },
-        Error => {
-            println!("err: {:#?}", &resp);
-        }
+    match JsonResult::Dog {
+        JsonResult::Dog => println!("dog image url: {:#?}", &resp),
+        JsonResult::Error => panic!("json response has error: {:#?}", &resp)
     }
     Ok(())
 }
