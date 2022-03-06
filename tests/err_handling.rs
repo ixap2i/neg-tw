8895c4431c4db44a0724342db304744b9e385585
ixap2i <ayamashita124@gmail.com>
2022-03-06 18:10:53 +0900

    remove unnecessary enum

--- tests/err_handling.rs
+++ tests/err_handling.rs
@@ -10,11 +10,6 @@ pub struct Dog {
     pub status: String,
 }
 
-pub enum JsonResult {
-    Dog,
-    Error
-}
-
 impl fmt::Display for Dog {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "json response has error")
@@ -38,14 +33,19 @@ fn type_of<T>(_: T) -> &'static str {
 
 #[tokio::test]
 async fn error_handling() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://dog.ceo/api/breeds/image/rando")
+    let resp = reqwest::get("https://dog.ceo/api/breeds/image/random")
         .await?
         .json::<Dog>()
         .await?;
 
-    match JsonResult::Dog {
-        JsonResult::Dog => println!("dog image url: {:#?}", &resp),
-        JsonResult::Error => panic!("json response has error: {:#?}", &resp)
+    if &resp.status == "success" {
+        println!("we got dog image url: {:#?}", &resp.message);
+    } else if &resp.status == "error" {
+        println!("json request occurred: {:#?}", &resp.message);
+    } else {
+        panic!("unexpected something error occurred : {:#?}",
+               &resp);
     }
     Ok(())
 }
+
